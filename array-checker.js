d2e41c085ac554eda9cf5a87dfb066a4de7cc956
Garinyr <garinyr92@gmail.com>
2022-09-13 22:09:45 +0700

    rename file

---
+++ array-checker.js
@@ -0,0 +1,47 @@
+function validate(arrPertama, arrKedua) {
+  let duplicate = [];
+
+  console.log('arrPertama');
+  console.log(arrPertama);
+  console.log('arrKedua');
+  console.log(arrKedua);
+
+  for (let i = 0; i < arrPertama.length; i++) {
+    const elPertama = arrPertama[i];
+
+    for (let j = 0; j < arrKedua.length; j++) {
+      const elKedua = arrKedua[j];
+
+      if (elPertama === elKedua) {
+        duplicate.push(elPertama);
+      }
+    }
+  }
+  
+  console.log('duplicate');
+  console.log(duplicate);
+}
+
+const myArr = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  'kita',
+  'bisa',
+  'kitabisa',
+  '1',
+  '2',
+  '3',
+];
+const myArr2 = ['a', 'c', 'e', 'a', 8, 1, 1, '3', 'kita', 'hello', 'kita'];
+
+validate(myArr, myArr2);
